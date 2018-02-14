527bd3a493590dd1d7096a28153b6e9dc743a01b
myshy93 <mihai.dinu93@gmail.com>
2018-02-14 14:01:34 +0200

    undo daemon for tickets threat

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import threads
 
 main_window = threads.Gui()
 main_window.start()  # porneste gui si continua codul
- # se conecteaza la db si pregateste url pentru a fi apelat prin pull_tickets
 whatcherObj = threads.Watcher()
 whatcherObj.daemon = True
 whatcherObj.start()
