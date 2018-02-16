b82b016295fbaf85183645f30068e6b9f3320632
myshy93 <mihai.dinu93@gmail.com>
2018-02-16 14:21:14 +0200

    Solved alert condition and built basic gui

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import threads
 
 main_window = threads.Gui()
 main_window.start()  # porneste gui si continua codul
-whatcherObj = threads.Watcher()
-whatcherObj.daemon = True
-whatcherObj.start()
+
+
+
 
 
 
--- threads.py
+++ threads.py
@@ -5,18 +5,40 @@ import tkinter.messagebox
 import threading
 
 
-class Gui(threading.Thread):
-    def run(self):
-        root = Tk()
-        root.mainloop()
-
-
 class Watcher(threading.Thread):
     group_tickets = ticketList.Tickets()
-    stop = 0
-    def run(self):
-        while self.stop == 0:
-            self.group_tickets.pull_tickets()
-            print(self.group_tickets.get_tks_ids_list())
-            time.sleep(20)
 
+    def run(self):
+
+        prev_tk_id = 0
+
+        while True:
+            self.group_tickets.pull_tickets()
+            tks_list = self.group_tickets.get_tks_ids_list()
+            tks_no = len(tks_list)
+
+            if tks_no is 0:
+                prev_tk_id = 0
+
+            elif prev_tk_id is 0 and tks_no is 1:
+                prev_tk_id = tks_list[0]
+                self.alert()
+
+            time.sleep(10)
+
+    def alert(self):
+        print("alert")
+
+
+class Gui(threading.Thread):
+
+    def run(self):
+        whatcherObj = Watcher()
+        whatcherObj.daemon = True
+        root = Tk()
+        root.title("TicketsWatcher")
+        mainframe = Frame(root, width=250, height=100)
+        mainframe.pack()
+        start_btn = Button(mainframe, text="START", command=whatcherObj.start)
+        start_btn.pack()
+        root.mainloop()
